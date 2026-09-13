3a3fbe5695f65f0d0cc5fe85a28526a46720b426
IT26101656 <it26101656@my.sliit.lk>
2026-09-14 02:14:16 +0530

    Add files via upload

---
+++ Q1/IT26101656Lab7Q1A.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class IT26101656Lab7Q1A {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter marks for four subjects: ");
+		
+		System.out.print("Enter mark 1: ");
+		double mark1 = input.nextDouble();
+		
+		
+		System.out.print("Enter mark 2: ");
+		double mark2 = input.nextDouble();
+		
+		
+		System.out.print("Enter mark 3: ");
+		double mark3 = input.nextDouble();
+		
+		
+		System.out.print("Enter marks 4: ");
+		double mark4 = input.nextDouble();
+		
+		
+		double average = (mark1 + mark2 + mark3 + mark4) / 4;
+		
+		
+		System.out.println("Average: " + average);
+		
+		
+		if (average >= 75) {
+			System.out.println("Grade: Distinction");
+			
+		}
+		else if (average >= 50) {
+			System.out.println("Grade: Credit");
+			
+		}
+		else {
+			System.out.println("Grade: Fail");
+			
+		}
+	}
+}
