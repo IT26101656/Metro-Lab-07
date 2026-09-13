aae851de6244009074eb5427accd79903b8dd434
IT26101656 <it26101656@my.sliit.lk>
2026-09-14 03:00:59 +0530

    Add files via upload

---
+++ Q2/IT26101656Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT26101656Lab7Q2A {
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 4; row++) {
+
+            for (int column = 1; column <= 5; column++) {
+                System.out.print("$ ");
+            }
+
+            System.out.println();
+        }
+    }
+}
