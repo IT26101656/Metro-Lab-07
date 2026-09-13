96aac4a2f05c1533cc0d18943e12e859477984e8
IT26101656 <it26101656@my.sliit.lk>
2026-09-14 03:02:03 +0530

    Add files via upload

---
+++ Q2/IT26101656Lab7Q2C.java
@@ -0,0 +1,13 @@
+public class IT26101656Lab7Q2C {
+    public static void main(String[] args) {
+
+        for (int number = 5; number >= 1; number--) {
+
+            for (int count = 1; count <= number; count++) {
+                System.out.print(number);
+            }
+
+            System.out.println();
+        }
+    }
+}
